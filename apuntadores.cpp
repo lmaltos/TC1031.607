d5107775c0f0a70b94471aa6c1505c6c8bc3afb4
lmaltos <136147644+lmaltos@users.noreply.github.com>
2023-09-19 14:58:26 +0000

    Reglas de decalogo memoria dinamica

--- apuntadores.cpp
+++ apuntadores.cpp
@@ -22,6 +22,58 @@ void test1() {
     cout << "p = " << p << endl;
 }
 
+// Regla 1 por cada new un delete    
+void Decalogo01() {
+    int *p = new int;
+    *p = 12;
+    delete p;
+    cout << "Despues de delete p = " << p << endl;
+    cout << "*p = " << *p << endl;
+    // ¿Qué pasa si borramos nuevamete p?
+    delete p;
+    cout << "Despues del segundo delete p = " << p << endl;
+    cout << "*p = " << *p << endl;
+}
+
+// Regal 2 un DELETE libera el espacio independientemente de la cantidad de apntadores
+void Decalogo02() {
+    int *p, *q, *r;
+    p = new int;
+    cout << "p = " << p << endl;
+    cout << "q = " << q << endl;
+    cout << "r = " << r << endl;
+    q = r = p;
+    delete r;
+    cout << "Despues del delete" << endl;
+    cout << "p = " << p << endl;
+    cout << "q = " << q << endl;
+    cout << "r = " << r << endl;
+    //delete q;
+}
+
+string *test3() {
+    string *p = new string;
+    *p = "hola";
+    cout << "&p = " << &p << endl;
+    cout << "p = " << p << endl;
+    cout << "*p = " << *p << endl;
+    return p;
+}
+// Regla 3
+void Decalogo03() {
+    string *q;
+    q = test3();
+    cout << "Despues de la llamada" << endl;
+    cout << "&q = " << &q << endl;
+    cout << "q = " << q << endl;
+    cout << "*q = " << *q << endl;
+    delete q;
+    cout << "Despues de delete" << endl;
+    cout << "&q = " << &q << endl;
+    cout << "q = " << q << endl;
+    cout << "*q = " << *q << endl;
+}
+
 int main() {
-    test1();
+    Decalogo03();
 }
